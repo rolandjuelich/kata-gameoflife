508399e3f0f5726c2059aba3093add9fd7970d8c
Roland Jülich <rolandjuelich@gmail.com>
2020-02-07 09:00:14 +0100

    initial checkin

---
+++ src/test/java/io/rjuelich/kata/gameoflife/GameOfLifeShould.java
@@ -0,0 +1,12 @@
+package io.rjuelich.kata.gameoflife;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class GameOfLifeShould {
+
+	@Test
+	public void doSomething() {
+		Assertions.fail("implement me");
+	}
+}
